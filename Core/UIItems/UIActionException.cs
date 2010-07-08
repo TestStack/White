c8b520788665e6520e99d8402f394eb15b1c41d0
arvind.sv <arvind.sv@ecd27aaf-8644-0410-a1a0-1db6a312f109>
2010-07-08 05:53:12 +0000

    Custom commands, on failure, sometimes try to serialize UIActionException. So, marking it serializable. <arvind>
    git-svn-id: https://white-project.googlecode.com/svn/trunk@77 ecd27aaf-8644-0410-a1a0-1db6a312f109

--- Core/UIItems/UIActionException.cs
+++ Core/UIItems/UIActionException.cs
@@ -3,6 +3,7 @@ using System;
 namespace White.Core.UIItems
 {
     //TODO: Change NUnit code to provide aspects, Checkout NUnit code and fix it
+    [Serializable]
     public class UIActionException : Exception
     {
         public UIActionException(string message) : base(message) {}
