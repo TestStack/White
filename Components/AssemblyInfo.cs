8593a447ee3b50f9a7de5b557625cc16db2e1cc2
unknown <vsingh@.corporate.thoughtworks.com>
2011-08-01 12:06:36 +0530

    dist without ndoc is working

--- Components/AssemblyInfo.cs
+++ Components/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly : AssemblyVersion("0.2.0.0")]
-[assembly : AssemblyFileVersion("0.2.0.0")]
+[assembly : AssemblyVersion("0.2.1.0")]
+[assembly : AssemblyFileVersion("0.2.1.0")]
