d982e8932b976ce2df720bcab832ba895da39a13
Jake Ginnivan <jake.ginnivan@readify.net>
2012-09-15 15:45:07 +1000

    Moved code into src folder

--- b32bit.bat
+++
@@ -1,2 +0,0 @@
-call tools\nant\NAnt.exe -nologo -buildfile:build\custom-nant.build compile.custom.nant -logfile:a.log -D:processor-type=32bit
-call w %*
--- w32bit.bat
+++
@@ -1,1 +0,0 @@
-call tools\nant\NAnt.exe -nologo -buildfile:build\master.build %* -logfile:w.log -D:processor-type=32bit
