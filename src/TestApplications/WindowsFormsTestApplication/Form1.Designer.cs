69bfc802dcf579f1fd0028422288be6768cec5c3
Jake Ginnivan <jake.ginnivan@readify.net>
2013-02-06 11:42:12 +0800

    Got the new UI tests project running

--- src/TestApplications/WindowsFormsTestApplication/Form1.Designer.cs
+++ src/TestApplications/WindowsFormsTestApplication/Form1.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.AComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // AComboBox
+            // 
+            this.AComboBox.FormattingEnabled = true;
+            this.AComboBox.Items.AddRange(new object[] {
+            "Test1",
+            "Test2"});
+            this.AComboBox.Location = new System.Drawing.Point(12, 12);
+            this.AComboBox.Name = "AComboBox";
+            this.AComboBox.Size = new System.Drawing.Size(121, 21);
+            this.AComboBox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 405);
+            this.Controls.Add(this.AComboBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox AComboBox;
     }
 }
 
