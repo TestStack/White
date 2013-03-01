678b77644a401527d94358fb91f5be58ead90698
Ritro <piranuy@gmail.com>
2013-03-01 21:50:02 +0400

    ADD: test for clicking Menu and test for DataGrid manipulations from custom child component for WpfApplication ADD: menu in WpfApplication FIX: Castle version updated FIX: DataGrid test divided into two separate files (for wpf and winforms)

---
+++ src/TestStack.White.UITests/ControlTests/WPF/MenuTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using TestStack.White.UITests.Infrastructure;
+using White.Core.UIItems.Finders;
+using White.Core.UIItems.WindowStripControls;
+
+namespace TestStack.White.UITests.ControlTests.WPF
+{
+    public class MenuTests : WhiteTestBase
+    {
+        protected MenuBar MenuUnderTest { get; set; }
+
+        protected override void RunTest(FrameworkId framework)
+        {
+            MenuUnderTest = MainWindow.Get<MenuBar>(SearchCriteria.ByAutomationId("MenuBar"));
+
+            RunTest(CanClickMenuItemTwice);
+        }
+
+        public void CanClickMenuItemTwice()
+        {
+            var firstMenu = MenuUnderTest.TopLevelMenu[0];
+            Assert.IsNotNull(firstMenu);
+            // to make menu items visible
+            firstMenu.Click();
+
+            var menuitem = firstMenu.SubMenu("ClickMe");
+            Assert.IsNotNull(menuitem);
+            menuitem.Click();
+            Assert.AreEqual("Clicked 1", menuitem.Name);
+
+            firstMenu.Click();
+            Thread.Sleep(1000);
+            menuitem.Click();
+            Assert.AreEqual("Clicked 2", menuitem.Name);
+
+            firstMenu.Click();
+            Thread.Sleep(1000);
+            menuitem.Click();
+            Assert.AreEqual("Clicked 3", menuitem.Name);
+        }
+
+        protected override IEnumerable<FrameworkId> SupportedFrameworks()
+        {
+            yield return FrameworkId.Wpf;
+        }
+    }
+}
