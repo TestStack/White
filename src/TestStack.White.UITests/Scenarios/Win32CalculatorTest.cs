51aaa5dacdc30dfec18c060b71aefdf6199801dd
Jake Ginnivan <jake.ginnivan@readify.net>
2013-03-26 13:51:50 +0800

    Added calculator example from http://www.codeproject.com/Articles/289028/White-An-UI-Automation-tool-for-windows-applicatio

---
+++ src/TestStack.White.UITests/Scenarios/Win32CalculatorTest.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+using White.Core;
+using White.Core.Factory;
+using White.Core.UIItems;
+using White.Core.UIItems.Finders;
+using White.Core.UIItems.ListBoxItems;
+using White.Core.UIItems.MenuItems;
+using White.Core.WindowsAPI;
+
+namespace TestStack.White.UITests.Scenarios
+{
+    [TestFixture]
+    public class Win32CalculatorTest
+    {
+        private const string ExeSourceFile = @"C:\Windows\system32\calc.exe";
+
+        [Test]
+        public void CalculatorTests()
+        {
+            //strat process for the above exe file location
+            var psi = new ProcessStartInfo(ExeSourceFile);
+            // launch the process through white application
+            using (var application = Application.AttachOrLaunch(psi))
+            using (var mainWindow = application.GetWindow(SearchCriteria.ByText("Calculator"), InitializeOption.NoCache))
+            {
+                mainWindow.Keyboard.HoldKey(KeyboardInput.SpecialKeys.CONTROL);
+                mainWindow.Keyboard.Enter("E");
+                mainWindow.Keyboard.LeaveKey(KeyboardInput.SpecialKeys.CONTROL);
+
+                //On Date window find the difference between dates.
+                //Set value into combobox
+                mainWindow.Get<ComboBox>(SearchCriteria.ByAutomationId("4003")).Select("Calculate the difference between two dates");
+                //Click on Calculate button
+                mainWindow.Get<Button>(SearchCriteria.ByAutomationId("4009")).Click();
+
+                mainWindow.Keyboard.HoldKey(KeyboardInput.SpecialKeys.CONTROL);
+                mainWindow.Keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.F4);
+                mainWindow.Keyboard.LeaveKey(KeyboardInput.SpecialKeys.CONTROL);
+
+                var menuView = mainWindow.Get<Menu>(SearchCriteria.ByText("View"));
+                menuView.Click();
+                var menuViewBasic = mainWindow.Get<Menu>(SearchCriteria.ByText("Basic"));
+                menuViewBasic.Click();
+
+                PerformSummationOnCalculator(mainWindow);
+            }
+        }
+
+        /// <summary>
+        /// method to Perform Addition of two numbers and validate the result
+        /// </summary>
+        private static void PerformSummationOnCalculator(UIItemContainer mainWindow)
+        {
+            mainWindow.Get<Button>(SearchCriteria.ByText("1")).Click();
+            mainWindow.Get<Button>(SearchCriteria.ByText("2")).Click();
+            mainWindow.Get<Button>(SearchCriteria.ByText("3")).Click();
+            mainWindow.Get<Button>(SearchCriteria.ByText("4")).Click();
+            mainWindow.Get<Button>(SearchCriteria.ByText("Add")).Click();
+            mainWindow.Get<Button>(SearchCriteria.ByText("5")).Click();
+            mainWindow.Get<Button>(SearchCriteria.ByText("6")).Click();
+            mainWindow.Get<Button>(SearchCriteria.ByText("7")).Click();
+            mainWindow.Get<Button>(SearchCriteria.ByText("8")).Click();
+            //Button with text as +(for sum)
+            //Read button to get the result
+            mainWindow.Get<Button>(SearchCriteria.ByText("Equals")).Click();
+
+            //Get the result
+            var resultLable = mainWindow.Get<Label>(SearchCriteria.ByAutomationId("150"));
+            string result = resultLable.Text;
+            Assert.AreEqual("6912", result);
+        }
+    }
+}
