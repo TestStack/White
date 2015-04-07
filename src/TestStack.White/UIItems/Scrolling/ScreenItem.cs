e88de4ce4969b3af5d01fd1e3fb4db27354075ec
FjFFj <klaus.guetter@it-gmbh.de>
2015-04-07 11:35:10 +0200

    Fix: previous change broke WPF ComboBox scrolling

--- src/TestStack.White/UIItems/Scrolling/ScreenItem.cs
+++ src/TestStack.White/UIItems/Scrolling/ScreenItem.cs
@@ -31,7 +31,38 @@ namespace TestStack.White.UIItems.Scrolling
                 return;
             }
 
-            if (verticalSpanProvider is UIItem)
+            if (verticalScroll != null && verticalScroll.IsScrollable)
+            {
+                logger.DebugFormat("Trying to make visible {0}, item's bounds are {1} and parent's span is {2}", uiItem, uiItem.Bounds, verticalSpan);
+
+                if (verticalScroll.IsNotMinimum)
+                {
+                    verticalScroll.SetToMinimum();
+                    verticalSpan = verticalSpanProvider.VerticalSpan;
+                    logger.DebugFormat("Scroll Position set to minimum value.");
+                }
+
+                if (verticalSpan.Contains(uiItem.Bounds))
+                {
+                    logger.DebugFormat("UIItem ({0}) whose bounds are ({1}) is within bounds of parent whose vertical span is {2}", uiItem,
+                                                     uiItem.Bounds, verticalSpan);
+                    return;
+                }
+
+                var success = Retry.For(
+                    () =>
+                    {
+                        verticalScroll.ScrollDownLarge();
+                        var bounds = uiItem.Bounds;
+                        const string messageFormat = "Trying to make {0} visible, item's bounds are {1} and parent's span is {2}";
+                        logger.DebugFormat(messageFormat, uiItem, bounds, verticalSpan);
+                        return verticalSpan.Contains(bounds);
+                    }, CoreAppXmlConfiguration.Instance.BusyTimeout(), TimeSpan.FromMilliseconds(0));
+
+                if (!success)
+                    throw new UIActionException(string.Format("Could not make the {0} visible{1}", uiItem, Constants.BusyMessage));
+            }
+            else if (verticalSpanProvider is UIItem)
             {
                 var containerUiItem = (UIItem)verticalSpanProvider;
 
@@ -44,26 +75,6 @@ namespace TestStack.White.UIItems.Scrolling
                             containerUiItem.MouseWheel(1);
                         else
                             containerUiItem.MouseWheel(-1);
-                        var bounds = uiItem.Bounds;
-                        const string messageFormat = "Trying to make {0} visible, item's bounds are {1} and parent's span is {2}";
-                        logger.DebugFormat(messageFormat, uiItem, bounds, verticalSpan);
-                        return verticalSpan.Contains(bounds);
-                    }, CoreAppXmlConfiguration.Instance.BusyTimeout(), TimeSpan.FromMilliseconds(0));
-
-                if (!success)
-                    throw new UIActionException(string.Format("Could not make the {0} visible{1}", uiItem, Constants.BusyMessage));
-            }
-            else if (verticalScroll != null && verticalScroll.IsScrollable)
-            {
-                logger.DebugFormat("Trying to make visible {0}, item's bounds are {1} and parent's span is {2}", uiItem, uiItem.Bounds, verticalSpan);
-
-                var success = Retry.For(
-                    () =>
-                    {
-                        if (uiItem.Bounds.Top + uiItem.Bounds.Bottom < verticalScroll.Bounds.Top + verticalScroll.Bounds.Bottom)
-                            verticalScroll.ScrollUpLarge();
-                        else
-                            verticalScroll.ScrollDownLarge();
                         var bounds = uiItem.Bounds;
                         const string messageFormat = "Trying to make {0} visible, item's bounds are {1} and parent's span is {2}";
                         logger.DebugFormat(messageFormat, uiItem, bounds, verticalSpan);
