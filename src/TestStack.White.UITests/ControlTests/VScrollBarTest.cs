d21a1de923b08cc37e6341c38625785f2e43cb4c
Max Young <max.young@lcs.com>
2015-05-15 23:26:29 -0400

    Reverted VScrollBarTest changes so I can just upload my treeview fix.

--- src/TestStack.White.UITests/ControlTests/VScrollBarTest.cs
+++ src/TestStack.White.UITests/ControlTests/VScrollBarTest.cs
@@ -10,53 +10,11 @@ namespace TestStack.White.UITests.ControlTests
     {
         private ListBox listBox;
         private IVScrollBar vScrollBar;
-        private WindowsFramework windowsFramework;
-
-        private const int SmallScrollCount = 6;
         private double smallChange;
-        private double SmallChange ()
-        {
-            double value = 0.0;
-            if (windowsFramework == WindowsFramework.Wpf)
-                value = smallChange;
-            else if (windowsFramework == WindowsFramework.WinForms)
-                if (SmallScrollCount % 2 == 0)
-                {
-                    value = (SmallScrollCount * 1.5) / SmallScrollCount;
-                }
-                else
-                {
-                    value = ((smallChange * SmallScrollCount) + 2) / SmallScrollCount;
-                }
-
-            return value;
-        }
-
-        private const int LargeScrollCount = 4;
         private double largeChange;
-        private double LargeChange ()
-        {
-            double value = 0.0;
-            if (windowsFramework == WindowsFramework.Wpf)
-                value = largeChange;
-            else if (windowsFramework == WindowsFramework.WinForms)
-                if (LargeScrollCount % 2 == 0)
-                {
-                    value = (LargeScrollCount * 9.5) / LargeScrollCount;
-                }
-                else
-                {
-                    value = ((largeChange * LargeScrollCount) + 2) / LargeScrollCount;
-                }
-
-            return value;
-        }
-
-
 
         protected override void ExecuteTestRun(WindowsFramework framework)
         {
-            windowsFramework = framework;
             listBox = MainWindow.Get<ListBox>("ListBoxWithVScrollBar");
             vScrollBar = listBox.ScrollBars.Vertical;
             vScrollBar.ScrollDown();
@@ -83,45 +41,42 @@ namespace TestStack.White.UITests.ControlTests
         void ShouldScrollDown()
         {
             double currentValue = vScrollBar.Value;
-            for (int scrollCount = 0; scrollCount < SmallScrollCount; scrollCount++)
-                vScrollBar.ScrollDown();
-
-            Assert.Equal(currentValue + (SmallChange() * SmallScrollCount), vScrollBar.Value, 3);
+            vScrollBar.ScrollDown();
+            vScrollBar.ScrollDown();
+            vScrollBar.ScrollDown();
+            vScrollBar.ScrollDown();
+            vScrollBar.ScrollDown();
+            Assert.Equal(currentValue + (smallChange*5), vScrollBar.Value, 3);
         }
 
         void ShouldScrollDownLarge()
         {
             double currentValue = vScrollBar.Value;
-            for (int scrollCount = 0; scrollCount < LargeScrollCount; scrollCount++)
-                vScrollBar.ScrollDownLarge();
-
-            Assert.Equal(currentValue + (LargeChange() * LargeScrollCount), vScrollBar.Value, 3);
+            vScrollBar.ScrollDownLarge();
+            vScrollBar.ScrollDownLarge();
+            vScrollBar.ScrollDownLarge();
+            Assert.Equal(currentValue + (largeChange*3), vScrollBar.Value, 3);
         }
 
         void ShouldScrollUp()
         {
             vScrollBar.SetToMaximum();
-            double maxValue = 0.0;
-
-            if (windowsFramework == WindowsFramework.Wpf)   
-                maxValue = vScrollBar.Value;
-            else if (windowsFramework == WindowsFramework.WinForms)
-                maxValue = vScrollBar.Value - 1;
-
-            for (int scrollCount = 0; scrollCount < SmallScrollCount; scrollCount++)
-                vScrollBar.ScrollUp();
-
-            Assert.Equal(maxValue - (SmallChange() * SmallScrollCount), vScrollBar.Value, 3);
+            double maxValue = vScrollBar.Value;
+            vScrollBar.ScrollUp();
+            vScrollBar.ScrollUp();
+            vScrollBar.ScrollUp();
+            vScrollBar.ScrollUp();
+            vScrollBar.ScrollUp();
+            Assert.Equal(maxValue - (smallChange * 5), vScrollBar.Value, 3);
         }
 
         void ShouldScrollUpLarge()
         {
             var currentValue = vScrollBar.Value;
-
-            for (int scrollCount = 0; scrollCount < LargeScrollCount; scrollCount++)
-                vScrollBar.ScrollUpLarge();
-
-            Assert.Equal(currentValue - (LargeChange() * LargeScrollCount), vScrollBar.Value, 3);
+            vScrollBar.ScrollUpLarge();
+            vScrollBar.ScrollUpLarge();
+            vScrollBar.ScrollUpLarge();
+            Assert.Equal(currentValue - (largeChange * 3), vScrollBar.Value, 3);
         }
 
         protected override IEnumerable<WindowsFramework> SupportedFrameworks()
