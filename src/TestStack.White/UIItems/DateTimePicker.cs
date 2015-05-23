49f678aca5f991c217af23e0902f8096d52c9ede
Max Young <max.young@lcs.com>
2015-05-23 01:43:55 -0400

    Revert "Fixed DatePickertest for winforms."
    This reverts commit 8913cfd99e85c262c9d5b4ac737c335f6e97d716.

--- src/TestStack.White/UIItems/DateTimePicker.cs
+++ src/TestStack.White/UIItems/DateTimePicker.cs
@@ -16,10 +16,6 @@ namespace TestStack.White.UIItems
             get
             {
                 var property = (string) Property(ValuePattern.ValueProperty);
-
-                if (string.IsNullOrEmpty(property))
-                    property = (string) Property(LegacyIAccessiblePattern.ValueProperty);
-
                 if (string.IsNullOrEmpty(property))
                     return null;
                 return DateTime.Parse(property);
