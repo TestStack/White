fa093c156c26dcac3277da7007983a577143ad4e
Johan Larsson <johan_gavle1@hotmail.com>
2015-12-29 18:29:46 +0100

    Update UIItem.cs
    Add virtual

--- src/TestStack.White/UIItems/UIItem.cs
+++ src/TestStack.White/UIItems/UIItem.cs
@@ -350,7 +350,7 @@ namespace TestStack.White.UIItems
 
         public virtual string HelpText { get { return automationElement.Current.HelpText; } }
 
-        public string ItemStatus { get { return automationElement.Current.ItemStatus; } }
+        public virtual string ItemStatus { get { return automationElement.Current.ItemStatus; } }
 
         /// <summary>
         /// Internal to white and intended to be used for white recording
