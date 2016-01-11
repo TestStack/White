2a2548fc9567670c18971b91f7f5809bc6e53607
Daniel P <stacatogit@gmail.com>
2016-01-11 10:57:22 +0100

    Revert "Cleaning up Mouse / IMouse"
    This reverts commit ec2128d1ceb324d8e4b2e69fa5e1fddbba3b2e44.

--- src/TestStack.White/InputDevices/AttachedMouse.cs
+++ src/TestStack.White/InputDevices/AttachedMouse.cs
@@ -8,169 +8,62 @@ namespace TestStack.White.InputDevices
     /// Any operation performed using the mouse would wait till the window is busy after this operation. Before any operation is 
     /// performed the window, from which it was retreived, is brought to focus if it is not.
     /// </summary>
-    public class AttachedMouse : IBaseMouse
+    public class AttachedMouse : IMouse
     {
         private readonly IActionListener actionListener;
-        private readonly IMouse mouse;
+        private readonly Mouse mouse;
 
-        #region Constructor
-
-        internal AttachedMouse(IMouse mouse, IActionListener actionListener)
-        {
-            this.actionListener = actionListener;
-            this.mouse = mouse;
-        }
-
-        #endregion
-
-        /// <summary>
-        /// Implements <see cref="IBaseMouse.Location"/>
-        /// </summary>
         public virtual Point Location
         {
             get { return mouse.Location; }
             set { mouse.Location = value; }
         }
 
-        public MouseCursor Cursor
+        internal AttachedMouse(Mouse mouse, IActionListener actionListener)
         {
-            get { return mouse.Cursor; }
+            this.actionListener = actionListener;
+            this.mouse = mouse;
         }
 
-        #region Mouse Click
-
-        public void Click()
-        {
-            mouse.Click();
-            ActionPerformed();
-        }
-
-        public void Click(Point point)
-        {
-            mouse.Click(point, actionListener);
-        }
-
-        public void Click(MouseButton mouseButton)
-        {
-            mouse.Click(mouseButton, actionListener);
-        }
-
-        public void Click(MouseButton mouseButton, Point point)
-        {
-            mouse.Click(mouseButton, point, actionListener);
-        }
-
-        #endregion
-
-        #region Double Click
-
-        public void DoubleClick(Point point)
-        {
-            mouse.DoubleClick(point, actionListener);
-        }
-
-        public void DoubleClick(MouseButton mouseButton)
-        {
-            mouse.DoubleClick(mouseButton, actionListener);
-        }
-
-        public void DoubleClick(MouseButton mouseButton, Point point)
-        {
-            mouse.DoubleClick(mouseButton, point, actionListener);
-        }
-
-        #endregion
-
-        #region Right Click
-
-        public void RightClick()
+        public virtual void RightClick()
         {
             mouse.RightClick();
             ActionPerformed();
         }
 
-        public void RightClick(Point point)
+        public virtual void Click()
         {
-            mouse.RightClick(point, actionListener);
+            mouse.Click();
+            ActionPerformed();
         }
 
-        #endregion 
+        public virtual void DoubleClick(Point point)
+        {
+            mouse.DoubleClick(point);
+            ActionPerformed();
+        }
 
-        #region Drag And Drop
+        public virtual void Click(Point point)
+        {
+            mouse.Click(point);
+            ActionPerformed();
+        }
 
-        public void DragAndDrop(IUIItem draggedItem, IUIItem dropItem)
+        public virtual void DragAndDrop(IUIItem draggedItem, IUIItem dropItem)
         {
             mouse.DragAndDrop(draggedItem, dropItem);
             ActionPerformed();
         }
 
-        public void DragAndDrop(MouseButton mouseButton, IUIItem draggedItem, IUIItem dropItem)
-        {
-            mouse.DragAndDrop(mouseButton, draggedItem, dropItem);
-            ActionPerformed();
-        }
-
-        public void DragAndDrop(IUIItem draggedItem, Point startPosition, IUIItem dropItem, Point endPosition)
+        public virtual void DragAndDrop(IUIItem draggedItem, Point startPosition, IUIItem dropItem, Point endPosition)
         {
             mouse.DragAndDrop(draggedItem, startPosition, dropItem, endPosition);
             ActionPerformed();
         }
 
-        public void DragAndDrop(MouseButton mouseButton, IUIItem draggedItem, Point startPosition, IUIItem dropItem, Point endPosition)
+        private void ActionPerformed()
         {
-            mouse.DragAndDrop(mouseButton,  draggedItem, startPosition, dropItem, endPosition);
-            ActionPerformed();
+            actionListener.ActionPerformed(Action.WindowMessage);
         }
-
-        #endregion
-
-        #region Drag
-
-        public void DragHorizontally(IUIItem uiItem, int distance)
-        {
-            mouse.DragHorizontally(uiItem, distance);
-            ActionPerformed();
-        }
-
-        public void DragHorizontally(MouseButton mouseButton, IUIItem uiItem, int distance)
-        {
-            mouse.DragHorizontally(mouseButton, uiItem, distance);
-            ActionPerformed();
-        }
-
-        public void DragVertically(IUIItem uiItem, int distance)
-        {
-            mouse.DragVertically(uiItem, distance);
-            ActionPerformed();
-        }
-
-        public void DragVertically(MouseButton mouseButton, IUIItem uiItem, int distance)
-        {
-            mouse.DragVertically(mouseButton, uiItem, distance);
-            ActionPerformed();
-        }
-
-        #endregion
-
-        public void MoveOut()
-        {
-            mouse.MoveOut();
-            ActionPerformed();
-        }
-
-        public void Move(Point position)
-        {
-            mouse.Move(position);
-            ActionPerformed();
-        }
-
-        #region Protected
-
-        protected void ActionPerformed()
-        {
-            mouse.ActionPerformed(actionListener);
-        }
-
-        #endregion
     }
 }
--- src/TestStack.White/InputDevices/IMouse.cs
+++ src/TestStack.White/InputDevices/IMouse.cs
@@ -1,90 +1,16 @@
 using System.Windows;
-using TestStack.White.UIItems.Actions;
+using TestStack.White.UIItems;
 
 namespace TestStack.White.InputDevices
 {
-    public interface IMouse : IBaseMouse
+    public interface IMouse
     {
-        #region Mouse Click
-
-        /// <summary>
-        /// Perform a Click Operation
-        /// </summary>
-        /// <param name="point">Point where to Click on</param>
-        /// <param name="actionListener">ActionListener to use after Click Operation</param>
-        void Click(Point point, IActionListener actionListener);
-        
-        /// <summary>
-        /// Perform a Click Operation
-        /// </summary>
-        /// <param name="mouseButton">MouseButton to Click</param>
-        /// <param name="actionListener">ActionListener to use after Click Operation</param>
-        /// <remarks>
-        /// Clicks the specified mouse button. Makes sure to not accidentaly fire a double click
-        /// if called multiple times
-        /// </remarks>
-        void Click(MouseButton mouseButton, IActionListener actionListener);
-
-        /// <summary>
-        /// Perform a Click Operation
-        /// </summary>
-        /// <param name="mouseButton">MouseButton to Click</param>
-        /// <param name="point">Point to Click on</param>
-        /// <param name="actionListener">ActionListener to use after Click Operation</param>
-        /// <remarks>
-        /// Clicks the specified mouse button. Makes sure to not accidentaly fire a double click
-        /// if called multiple times
-        /// </remarks>
-        void Click(MouseButton mouseButton, Point point, IActionListener actionListener);
-
-        #endregion
-
-        #region Double Click
-
-        /// <summary>
-        /// Perform a Double Click Operation
-        /// </summary>
-        /// <param name="point">Point where to Double Click on</param>
-        /// <param name="actionListener">ActionListener to use after Click Operation</param>
-        void DoubleClick(Point point, IActionListener actionListener);
-
-        /// <summary>
-        /// Perform a Double Click Operation
-        /// </summary>
-        /// <param name="mouseButton">MouseButton to double click</param>
-        /// <param name="actionListener">ActionListener to use after Click Operation</param>
-        void DoubleClick(MouseButton mouseButton, IActionListener actionListener);
-
-        /// <summary>
-        /// Perform a Double Click Operation
-        /// </summary>
-        /// <param name="mouseButton">MouseButton to double click</param>
-        /// <param name="point">Point where to Double Click on</param>
-        /// <param name="actionListener">ActionListener to use after Click Operation</param>
-        void DoubleClick(MouseButton mouseButton, Point point, IActionListener actionListener);
-
-        #endregion
-
-        #region Right Click
-
-        /// <summary>
-        /// Perform a Right Click Operation
-        /// </summary>
-        /// <param name="point">Point where to Double Click on</param>
-        /// <param name="actionListener">ActionListener to use after Click Operation</param>
-        void RightClick(Point point, IActionListener actionListener);
-
-        #endregion
-
-        #region Perform Action
-
-        /// <summary>
-        /// Perform an Action on the Action Listener 
-        /// </summary>
-        /// <param name="actionListener"><see cref="IActionListener"/> to perform an Action on</param>
-        void ActionPerformed(IActionListener actionListener);
-
-        #endregion
-
+        Point Location { get; set; }
+        void RightClick();
+        void Click();
+        void DoubleClick(Point point);
+        void Click(Point point);
+        void DragAndDrop(IUIItem draggedItem, IUIItem dropItem);
+        void DragAndDrop(IUIItem draggedItem, Point startPosition, IUIItem dropItem, Point endPosition);
     }
 }
