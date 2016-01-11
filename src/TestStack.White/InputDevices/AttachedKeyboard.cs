02830308e68e6ae32747eaa121f9450115e903de
Daniel P <stacatogit@gmail.com>
2016-01-11 09:53:56 +0100

    Cleaning up Keyboard
    Cleaning up the Keyboard components
    
    BareMetalKeyboard
    + This contains the DLLImports from user32
    + Basic Keyboard interaction and input validation
    
    IBaseKeyboard / BaseKeyboard
    + Moved alot of the Keyboard Functions and Properties from Keyboard to
    BaseKEyboard
    + BaseKeyboard handles anything not relevant to an IActionListener
    
    IKeyboard / Keyboard
    + Handles anything related to an IActionListener
    
    AttachedKeyboard
    + Implements IBaseKeyboard
    + Handles focusing on an UI Item Container before doing any keyboard
    interaction

--- src/TestStack.White/InputDevices/AttachedKeyboard.cs
+++ src/TestStack.White/InputDevices/AttachedKeyboard.cs
@@ -7,49 +7,94 @@ namespace TestStack.White.InputDevices
     /// Any operation performed using the keyboard would wait till the container is busy after this operation. Before any operation is 
     /// performed on the container, from which it was retreived, is brought to focus if it is not.
     /// </summary>
-    public class AttachedKeyboard : IKeyboard
+    public class AttachedKeyboard : IBaseKeyboard
     {
-        private readonly UIItemContainer container;
-        private readonly Keyboard keyboard;
+        #region Fields
 
-        internal AttachedKeyboard(UIItemContainer container, Keyboard keyboard)
+        private readonly UIItemContainer container;
+        private readonly IKeyboard keyboard;
+
+        #endregion
+
+        #region Constructor
+
+        internal AttachedKeyboard(UIItemContainer container, IKeyboard keyboard)
         {
             this.container = container;
             this.keyboard = keyboard;
         }
 
+        #endregion
+
+        /// <summary>
+        /// Implements <see cref="IBaseKeyboard.Enter(string)"/>
+        /// </summary>
         public virtual void Enter(string keysToType)
         {
             container.Focus();
             keyboard.Send(keysToType, container);
         }
 
+        /// <summary>
+        /// Implements <see cref="IBaseKeyboard.PressSpecialKey(KeyboardInput.SpecialKeys)"/>
+        /// </summary>
         public virtual void PressSpecialKey(KeyboardInput.SpecialKeys key)
         {
             container.Focus();
             keyboard.PressSpecialKey(key, container);
         }
 
+        /// <summary>
+        /// Implements <see cref="IBaseKeyboard.HoldKey(KeyboardInput.SpecialKeys)"/>
+        /// </summary>
         public virtual void HoldKey(KeyboardInput.SpecialKeys key)
         {
             container.Focus();
             keyboard.HoldKey(key, container);
         }
 
+        /// <summary>
+        /// Implements <see cref="IBaseKeyboard.LeaveKey(KeyboardInput.SpecialKeys)"/>
+        /// </summary>
         public virtual void LeaveKey(KeyboardInput.SpecialKeys key)
         {
             container.Focus();
             keyboard.LeaveKey(key, container);
         }
 
+        /// <summary>
+        /// Implements <see cref="IBaseKeyboard.CapsLockOn"/>
+        /// </summary>
         public virtual bool CapsLockOn
         {
-            get { return keyboard.CapsLockOn; }
+            get
+            {
+                return keyboard.CapsLockOn;
+            }
             set
             {
-                container.Focus();
                 keyboard.CapsLockOn = value;
             }
+        }
+
+        /// <summary>
+        /// Implements <see cref="IBaseKeyboard.HeldKeys"/>
+        /// </summary>
+        public virtual KeyboardInput.SpecialKeys[] HeldKeys
+        {
+            get
+            {
+                return keyboard.HeldKeys;
+            }
+        }
+
+        /// <summary>
+        /// Implements <see cref="IBaseKeyboard.LeaveAllKeys()"/>
+        /// </summary>
+        public virtual void LeaveAllKeys()
+        {
+            container.Focus();
+            keyboard.LeaveAllKeys();
         }
     }
 }
